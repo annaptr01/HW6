ac307455a09f0eaed38b052ad2562f7b71c7a782
anna <annetakaulitz@mail.ru>
2023-05-25 23:53:14 +0300

    add new code

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
 ﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
 //Напишите программу, которая покажет количество чётных чисел в массиве.
 //[345, 897, 568, 234] -> 2
+//1. задать 3 значность
+//2. задать положительность
+//3. рандомно заполнить массив 
+//4. Функция которая покажет количество четных чисел в массиве
 
+
+/*
 int [] CreateRandomArray(int size, int minVal, int maxVal)  //создала рандомный массив
 {
     int [] newArray = new int[size];
@@ -22,13 +28,12 @@ void EvenNumberFinder (int [] arrayForEven)
 {
     int count = 0;
     for(int i = 0; i < arrayForEven.Length; i++)
-        if (arrayForEven[i] > 99 && arrayForEven[i] < 1000)   //задаю трехзнычные числа и проверяю
-            count ++;
-        if (arrayForEven.Length % 2 == 0)     //проверка на четность
-            Console.WriteLine($"Even-numbered elements are: {count}");
-        else
-            Console.WriteLine("There are not even-numbered elements!");
-   
+       if (arrayForEven [i] % 2 == 0)     //проверка на четность
+           {
+               count += 1;
+               Console.WriteLine($"Even-numbered elements are: {count}");
+           }
+        
 }
 
 int sizeArray = 4;
@@ -38,16 +43,51 @@ int maxValue = 999;
 int [] createdArray = CreateRandomArray(sizeArray, minValue, maxValue);
 ShowArray(createdArray);
 EvenNumberFinder(createdArray);
+*/
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //Задача 36: Задайте одномерный массив, заполненный случайными числами. 
-//Найдите сумму элементов, стоящих на нечётных позициях.
+//Найдите сумму элементов, стоящих на нечётных позициях. (1,3)
 //[3, 7, 23, 12] -> 19
 //[-4, -6, 89, 6] -> 0
 
 
+int [] CreateRandomArray (int size, int minVal, int maxVal)  //создала рандомный массив со случайными числами
+{
+    int [] newArray = new int[size];
+    
+    for(int i = 0; i < size; i++)
+        newArray[i] = new Random().Next(minVal, maxVal + 1);
+    return newArray;
+}
+
+void ShowArray(int [] array1)  //вывести массив
+{
+    for(int i = 0; i < array1.Length; i++)
+       Console.Write(array1[i] + " ");
+    Console.WriteLine();
+}
+
+int FindSumNotEvenNum (int [] array1)  //поиск суммы чисел на нечетных позициях
+{
+    int notEvenNum = 0;
+    for(int i = 0; i < array1.Length; i++)
+       if(array1[i] % 2 != 0)
+         sum += array1[i];
+    return notEvenNum;
+}
+
+int sizeArray = 4;
+int minArray = -99;
+int maxArray = 99;
+
+int createdArray = CreateRandomArray (int sizeArray, int minArray, int maxArray);
+ShowArray(createdArray);
+Console.WriteLine($"The sum of not even-numbered elements are {FindSumNotEvenNum(createdArray)}");
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 //Задача 38: Задайте массив вещественных чисел. 
